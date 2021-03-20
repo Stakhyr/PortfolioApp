624edee70c707f947fdadb836687b9dd344fde28
Stakhyr <56436513+Stakhyr@users.noreply.github.com>
2021-03-20 16:04:24 +0200

    Add project files.

---
+++ PortfolioApp/Controllers/DepartmentController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PortfolioApp.Models;
+
+namespace PortfolioApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PortfolioAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department depart)
+        {
+            string query = @"
+                        insert into dbo.Department values ('" + depart.DepartmentName + @"')
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PortfolioAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfuly");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Department depart)
+        {
+            string query = @"
+                        update dbo.Department set
+                        DepartmentName = '" + depart.DepartmentName + @"'
+                        where DepartmentId = " + depart.DepartmentId + @"
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PortfolioAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfuly");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        delete from dbo.Department 
+                        where DepartmentId = " + id + @"
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PortfolioAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Delete Successfuly");
+        }
+    }
+}
---
+++ PortfolioApp/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }  
+
+        public string BirthDate { get; set; }
+
+        public string PortfolioFileName { get; set; }
+
+        public string UserRole { get; set; }
+
+    }
+}
